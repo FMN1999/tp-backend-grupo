5a449b51f36c592cb5926fc32c6c18a2f325a504
Guille23 <guillermo230899@gmail.com>
2022-06-08 16:35:17 -0300

    Método de actualización terminado y actualización de nombres de métodos

--- models/temporada/index.js
+++ models/temporada/index.js
@@ -30,9 +30,13 @@ module.exports.TemporadaAPI = (app) => {
     .post('/', TemporadaController.createTemporada)
 
     //Con esto genero una URL: http://localhost:3000/id
-    .post('/:id', TemporadaController.dropTemporada)
+    .delete('/:id', TemporadaController.deleteTemporada)
+
+    //Con esto genero una URL: http://localhost:3000/id
+    .put('/:id', TemporadaController.updateTemporada)
 
     app.use('/api/temporadas', router);
+
     //Aplicando router, en el caso del getTemporadas (Primer GET)
     // mi URL quedaría: http://localhost:3000/api/temporadas/
 
@@ -40,6 +44,10 @@ module.exports.TemporadaAPI = (app) => {
     //la URL quedaría: http://localhost:3000/api/temporadas/id
 
     //En el caso de POST, la URL queda: http://localhost:3000/api/temporadas/
+
+    //En el caso de PUT, http://localhost:3000/api/temporadas/id
+
+    //En el caso de DELETE, http://localhost:3000/api/temporadas/id
 
 
 }
