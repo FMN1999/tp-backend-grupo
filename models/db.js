bd54264e6a1e3446e6130a9b0ee4296bec8f0eda
Arielb11 <arielbuchhamer1@outlook.com>
2022-06-21 22:15:38 -0300

    Entidad precioRopa creada y terminada

--- models/db.js
+++ models/db.js
@@ -1,12 +1,9 @@
 const mongoose = require ("mongoose");
 const uri = "mongodb+srv://usuario:contraseña@ttads.tbykf.mongodb.net/tienda_ropa?retryWrites=true&w=majority";
 
-
-
 const dbConection = mongoose
     .connect(uri) 
     .then(() => console.log("Conectado a MongoDB Atlas")) 
     .catch((error) => console.error(error)); 
-
 
 module.exports = dbConection;
