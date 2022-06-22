24c128ab0869d8dc102eae18c4fe83e0ff361ab5
guillermodolan <guillermo230899@gmail.com>
2022-06-22 18:20:04 -0300

    Entidad temporada con pasaje a Mongoose

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-//Aca vamos a consumir cada uno de los módulos que agreguemos en 'models'
-
-
-//Obtengo el paquete de express
 const express = require('express');
 
+const mongoose = require('mongoose');
 
-//Traigo el paquete de debug. Esto servirá para el método de listen(), en la variable app.
-//El segundo paréntesis servirá para mostrar el nombre del archivo donde trabaja la configuración
-//del servidor, en este caso, este es el archivo
-const debug = require('debug')("app:main");
-
-
-const { TemporadaAPI } = require('./models/temporada');
-
-
-//Defino el puerto para la aplicación.
 const PORT = 3000;
 
+const tempoRutas = require('./models/temporada/controller');
+const dbConnection = require('./models/db');
 
-//Creo una aplicación 'express'
 const app = express();
 
-
-//Con esto le doy la capacidad a nuestro servidor de recibir datos en el cuerpo de la petición
-//que nos da. Esto nos servirá para el método POST
 app.use(express.json());
+app.use("/api", tempoRutas);
 
 
-
-TemporadaAPI(app);
-
-
-
-app.listen(PORT, () => {
-    debug(`Servidor escuchando en el puerto: ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Servidor escuchando: ${PORT}`));
--- models/db.js
+++ models/db.js
@@ -1,55 +1,9 @@
-//Importo el módulo de MongoClient por medio del método de require. Con las llaves ( {MongoClient} )
-//estoy realizando una desestructuración del objeto
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const dbConnection = mongoose
+    .connect(process.env.MONGODB_URI)
+    .then( () => console.log("Conexión a MongoDB Atlas exitosa") )
+    .catch( () => console.error(error) )
 
-const debug = require('debug')('app:module-database');
-
-
-//Esta variable es el nombre de la base de datos.
-const dbName = 'tienda_ropa';
-
-
-//Esta variable será para la conexión a la base de datos
-var conexion = null;
-
-
-
-//Creo un módulo llamado 'Database', al cual le paso como parámetro 'coleccion', que es la 
-//colección a la cual voy a acceder.
-module.exports.Database = (coleccion) => new Promise( async(resolve, reject) => {
-
-
-    //Dado que vamos a trabajar con código asíncrono, debe de usarse la cláusula try-catch
-    try{
-
-        //Verifico primero de si existe ya una conexión a la base de datos
-        if(!conexion){
-
-            //Creo la uri que luego servirá para crear el cliente de conexión
-            const uri = "mongodb+srv://usuario:contraseña@ttads.tbykf.mongodb.net/?retryWrites=true&w=majority";
-
-            //Genero un nuevo cliente de conexión. A este debo pasarle el uri.
-            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-            //Obtengo la conexión. Usamos await dado que la función connect() es asíncrona.
-            conexion = await client.connect();
-            debug("Nueva conexión con MongoDB Atlas");
-        }
-
-        debug('Reutilizando conexión');
-
-        //De existir una conexión abierta, entonces paso el nombre de la base de datos(creada más arriba).
-        const db = conexion.db(dbName);
-
-
-        //Aca 'resuelvo' esa colección a la cual quiero conectarme (la que recibo como parámetro en 
-        //el 'module.exports.Dabase'). Para esto uso resolve(). Esto me devolverá la colección 
-        //desde la base de datos
-        resolve(db.collection(coleccion))
-        
-    }catch(error) {
-        reject(error);
-    }
-
-});
+module.exports = dbConnection;
