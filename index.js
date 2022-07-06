490a7837f8ddaaceda81d89b14e1e696b7221d33
guillermodolan <guillermo230899@gmail.com>
2022-07-06 18:37:11 -0300

    Implementación de errores

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 //Importo el paquete de mongoose
 const mongoose = require('mongoose');
 
-const {IndexAPI} = require('./index/index');
+const {IndexAPI, NotFoundAPI} = require('./index/index');
 
 require('dotenv').config();
 
@@ -18,12 +18,15 @@ const app = express();
 
 app.use(express.json());
 
-
+//Debe de colocarse primero el menú principal
 IndexAPI(app);
+
+//Luego se colocan las demás entidades/rutas
 app.use("/api", precioRopaRutas);
 app.use("/api", tempoRutas);
 app.use("/api", ropaRutas);
 
-
+//Por último se coloca lo de errores
+NotFoundAPI(app);
 
 app.listen(process.env.PORT, () => console.log(`Servidor corriendo en: ${process.env.PORT}`));
