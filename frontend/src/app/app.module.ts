e5a1db8271c0187d0bc3777b3bca00375c7d2808
Matias <santolarimatias@gmail.com>
2022-07-10 21:15:44 -0300

    listado ropa frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RopasComponent } from './ropas/ropas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RopasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
