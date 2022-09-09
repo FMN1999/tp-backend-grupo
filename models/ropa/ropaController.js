e7ab27e3cf69e69f8d79e0602cc1b50ebfc2c104
FrancoNavarro <franconavarro1889@gmail.com>
2022-09-08 23:31:19 -0300

    mapear con metodo de busqueda

--- models/ropa/ropaController.js
+++ models/ropa/ropaController.js
@@ -144,7 +144,15 @@ router.get('/ropasFiltro/:detalleTipoRopa', async(req, res) => {
     }
 })
 
-
+router.get('/ropasSearch/:texto_busqueda', async(req, res) => {
+    try {
+        const {texto_busqueda} = req.params;
+        let ropas = await ropaModel.find({"detalle": new RegExp('.*' + texto_busqueda + '.*')})
+            .populate('tipoRopa')
+            .populate('precioRopa').populate('temporada');
+        Response.success(res, 200, 'Busqueda por texto: ', ropas);
+    } catch (error) {
+        Response.error(res);
 
 //Importo las rutas para usar desde el index.js, almacenado en la carpeta raíz del proyecto
 module.exports = router;
